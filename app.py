5cd4f8d5f34a8a84ead849697d36cdc7f05b188f
jiayong deng <jiayong@users.noreply.huggingface.co>
2023-12-26 09:54:43 +0000

    Update app.py
    gr share=False -> True

--- app.py
+++ app.py
@@ -417,7 +417,7 @@ with gr.Blocks(title = "Dreamoving",
 # concurrency_count, concurrency_limit, max_threads
 demo.queue(api_open=False, max_size=1000).launch(
     server_name="0.0.0.0", # if os.getenv('GRADIO_LISTEN', '') != '' else "127.0.0.1",
-    share=False,
+    share=True,
     server_port=7860,
     root_path=f"/{os.getenv('GRADIO_PROXY_PATH')}" if os.getenv('GRADIO_PROXY_PATH') else ""
 )
